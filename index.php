1fa6b44de399dcdb9e7f9210e322f5e451fc9833
Sandip Gopani <gopanisandip00@gmail.com>
2024-09-26 18:21:20 +0530

    bug fix

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
-echo $_SERVER['REQUEST_URI'];
 
-#if($_SERVER['REQUEST_URI'] ===)
+if($_SERVER['REQUEST_URI'] === '/'){
+    header('Location: https://ecypse.iljmp.com/1/beam-ltd');
+}else if($_SERVER['REQUEST_URI'] === '/fb-deal'){
+    header('Location: https://ecypse.iljmp.com/1/beam-ltd');
+}else{
+    echo "Comming soon";
+}
