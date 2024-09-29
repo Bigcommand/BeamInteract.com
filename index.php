2e3202ab2266070502e9ec2b7862216ca40be0de
Sandip Gopani <gopanisandip00@gmail.com>
2024-09-29 19:04:05 +0530

    bug fix

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 $url = explode('?', $_SERVER['REQUEST_URI'])[0] ?? 'not-found';
 
-if($url === '/'){
+if($url == '/'){
     header('Location: https://ecypse.iljmp.com/1/beam-ltd');
-}else if($url === '/fb-deal'){
+}else if($url == '/fb-deal'){
     header('Location: https://ecypse.iljmp.com/1/beam-ltd');
 }else{
     echo "Comming soon";
